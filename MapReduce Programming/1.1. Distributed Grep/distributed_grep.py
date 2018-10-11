5c7c03d54a65de80252b0deca8d43c849209c9e9
Unknown <ykushch@ucm.es>
2018-10-11 18:52:29 +0200

    Distributed Grep 0.2

--- MapReduce Programming/1.1. Distributed Grep/distributed_grep.py
+++ MapReduce Programming/1.1. Distributed Grep/distributed_grep.py
@@ -9,12 +9,12 @@ word = input();
 print('Where do you want me to look it for you?')
 print('(please include the file path and extension)')
 fileInput = input()
-file = open(fileInput, "rt")
+file = open(fileInput, "r")
 
 #lineNumber is used for showing the line number
 lineNumber = 1
 
-for line in file
+for line in file:
     #parsing lines and words
     parsedLine = re.sub(r'^\W+|\W+$', '', line)
     words = re.split(r"\W+", parsedLine)
@@ -22,9 +22,11 @@ for line in file
     #wordsInLine is used for prevention of duplicated lines
     wordsInLine = 0
 
-    for w in words
-        if w == word
-            if wordsInLine == 0
-                wordsInLine++
-                print("Line " + lineNumber + ": " + line)
-    lineNumber++
+    for w in words:
+        if w == word:
+            if wordsInLine == 0:
+                wordsInLine += 1
+                print("Line {0}: {1}".format(lineNumber, line))
+    lineNumber += 1
+
+file.close()
