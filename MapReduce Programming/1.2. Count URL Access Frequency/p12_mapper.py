863d47b6b363f1ab80a4585cfaa6c72a17d6c1a1
Unknown <ykushch@ucm.es>
2018-11-20 12:20:44 +0100

    Mejora código

--- MapReduce Programming/1.2. Count URL Access Frequency/p12_mapper.py
+++ MapReduce Programming/1.2. Count URL Access Frequency/p12_mapper.py
@@ -3,8 +3,18 @@
 import sys
 import re
 
-for line in sys.stdin:
-    line = re.sub( r'^\W+|\W+$', '', line )
-    part = line.split(" ")
+#######################################################
+# Parsea las lineas sacando las URL y poniendo un 1
+#######################################################
+def mapper():
+    for line in sys.stdin:
+        line = re.sub( r'^\W+|\W+$', '', line )
+        part = line.split(" ")
 
-    print( part[0] + "\t1" )
+        print( part[0] + "\t1" )
+
+def main():
+    mapper()
+
+if __name__ == "__main__":
+    main()
--- MapReduce Programming/1.2. Count URL Access Frequency/p12_reducer.py
+++ MapReduce Programming/1.2. Count URL Access Frequency/p12_reducer.py
@@ -2,18 +2,29 @@
 
 import sys
 
-previous = None
-sum = 0
+###########################################################
+# Recibe la entrada por stdin y calcula el numero de veces
+# que una url ha sido accedida
+###########################################################
+def reducer():
+    previous = None
+    sum = 0
 
-for line in sys.stdin:
-    key, value = line.split( '\t' )
+    for line in sys.stdin:
+        key, value = line.split( '\t' )
 
-    if key != previous:
-        if previous is not None:
-            print str( sum ) + '\t' + previous
-        previous = key
-        sum = 0
+        if key != previous:
+            if previous is not None:
+                print (str( sum ) + '\t' + previous)
+            previous = key
+            sum = 0
 
-    sum = sum + int( value )
+        sum = sum + int( value )
 
-print str( sum ) + '\t' + previous
+    print(str( sum ) + '\t' + previous)
+
+def main():
+    reducer()
+
+if __name__ == "__main__":
+    main()
