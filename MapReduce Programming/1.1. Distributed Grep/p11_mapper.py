c6c74e07080332fc7ec38f1feb7ec42993135ac7
Unknown <ykushch@ucm.es>
2018-11-20 12:12:47 +0100

    mejora de codigo

--- MapReduce Programming/1.1. Distributed Grep/p11_mapper.py
+++ MapReduce Programming/1.1. Distributed Grep/p11_mapper.py
@@ -3,12 +3,21 @@
 import sys
 import re
 
-w = sys.argv[1]
+###################################################################
+# Busca las lineas de texto donde aparece la palabra buscada
+###################################################################
+def mapper(w):
+    for line in sys.stdin:
+        line = re.sub( r'^\W+|\W+$', '', line )
+        words = re.split(r"\W+", line)
 
-for line in sys.stdin:
-    line = re.sub( r'^\W+|\W+$', '', line )
-    words = re.split(r"\W+", line)
+        for word in words:
+            if w == word:
+                print(word.lower() + "\t" + line)
 
-    for word in words:
-        if w == word:
-            print(word.lower() + "\t" + line)
+def main():
+    w = sys.argv[1]
+    mapper(w)
+
+if __name__ == "__main__":
+    main()
--- MapReduce Programming/1.1. Distributed Grep/p11_reducer.py
+++ MapReduce Programming/1.1. Distributed Grep/p11_reducer.py
@@ -2,11 +2,21 @@
 
 import sys
 
-previous = ""
-sum = ""
+#######################################################
+# Agrupa las líneas que le llegan por stdin
+#######################################################
+def reducer():
+    previous = ""
+    sum = ""
 
-for line in sys.stdin:
-    key, value = line.split( '\t' )
-    sum = sum + '\t' + value
+    for line in sys.stdin:
+        key, value = line.split( '\t' )
+        sum = sum + '\t' + value
 
-print(key + sum)
+    print(key + sum)
+
+def main():
+    reducer()
+
+if __name__ == "__main__":
+    main()
