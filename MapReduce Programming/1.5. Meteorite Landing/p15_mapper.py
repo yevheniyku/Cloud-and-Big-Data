bf3e35586f848add7b9df4f838e6c5ba86d333ab
Unknown <ykushch@ucm.es>
2018-11-23 23:32:25 +0100

    codigo mejorado

--- MapReduce Programming/1.5. Meteorite Landing/p15_mapper.py
+++ MapReduce Programming/1.5. Meteorite Landing/p15_mapper.py
@@ -3,18 +3,24 @@
 import sys
 import re
 
-firstline = True
+def mapper():
 
-for line in sys.stdin:
+    for line in sys.stdin:
 
-    line = re.sub( r'^\W+|\W+$', '', line )
-    info = line.split(",")
+        line = re.sub( r'^\W+|\W+$', '', line )
+        info = line.split(",")
 
-    meteoriteClass = info[3]
-    if(not any(char.isalpha() for char in info[4])):
-        meteoriteMass = info[4]
-    else:
-        meteoriteMass = info[5]
+        meteoriteClass = info[3]
+        if(not any(char.isalpha() for char in info[4])):
+            meteoriteMass = info[4]
+        else:
+            meteoriteMass = info[5]
 
-    if(meteoriteMass != ""):
-        print (meteoriteClass + '\t' + str(meteoriteMass))
+        if(meteoriteMass != ""):
+            print (meteoriteClass.lower() + '\t' + str(meteoriteMass))
+
+def main():
+    mapper()
+
+if __name__ == "__main__":
+    main()
--- MapReduce Programming/1.5. Meteorite Landing/p15_reducer.py
+++ MapReduce Programming/1.5. Meteorite Landing/p15_reducer.py
@@ -2,18 +2,32 @@
 
 import sys
 
-previous = None
-sum = 0
+##########################################################
+# funcion reducer agrupa los meteoritos por su clase
+# y saca la masa media de cada clase
+##########################################################
+def reducer():
+    previous = None
+    sum = 0
+    length = 1
 
-for line in sys.stdin:
-    key, value = line.split( '\t' )
+    for line in sys.stdin:
+        key, value = line.split( '\t' )
 
-    if key != previous:
-        if previous is not None:
-            print previous + '\t' + str( sum ) 
-        previous = key
-        sum = 0
+        if key != previous:
+            if previous is not None:
+                print("{}\t{}".format(previous, (sum/length)))
+            previous = key
+            sum = 0
+            length = 1
 
-    sum = sum + float( value )
+        sum = sum + float( value )
+        length += 1
 
-print  previous + '\t' + str( sum )
+    print("{}\t{}".format(previous, sum/length))
+
+def main():
+    reducer()
+
+if __name__ == "__main__":
+    main()
